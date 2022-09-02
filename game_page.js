26e6b05e7150b70f80b3d1b8039e4a695c9ed8f4
WhenPigsFly12 <99145987+WhenPigsFly12@users.noreply.github.com>
2022-09-01 18:40:55 -0700

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,44 @@
+player1_name = localStorage.getItem("player1_name")
+player2_name = localStorage.getItem("player2_name")
+player1_score = 0
+player2_score = 0
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn: " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn: " + player2_name;
+
+function send() {
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    console.log(word);
+
+    char1 = word.charAt(1);
+    console.log(char1);
+
+    length_div_2 = Math.floor(word.length/2);
+    char2 = word.charAt(length_div_2);
+
+    length_minus_1 = word.length - 1;
+    char3 = word.charAt(length_minus_1);
+
+    console.log(char2);
+    console.log(char3);
+
+    remove_char1 = word.replace(char1, "_");
+    remove_char2 = remove_char1.replace(char2, "_");
+    remove_char3 = remove_char2.replace(char3, "_");
+
+    question_word = "<h4 id='word_display'>Q. " + remove_char3 + "</h4>";
+    input_box = "<br> Answer: <input type='text' id='input_check_box'>";
+    check_button = "<br> <br> <button class='btn btn-info' onclick='check()'> Check </button>";
+    row = question_word + input_box + check_button;
+
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("input_check_box").value = "";
+
+}
